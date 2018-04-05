0cea2e41dfb822a1e72f0a87bfa47a516fdfa30d
ghoussard <houssard.gregoire@gmail.com>
2018-04-05 13:55:12 +0200

    homepage

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MainRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +16,11 @@ class IndexController extends AbstractController
     public function index(SemanticGui $gui){
     	$gui->getOnClick(".elements","", "#block-body",["attr"=>"data-ajax"]);
     	$gui->getOnClick("#menu a[data-ajax]","","#block-body",["attr"=>"data-ajax"]);
-        return $gui->renderView("index.html.twig");
+
+    	$models = array_map(function ($model) {
+    		return substr($model, 11);
+	    }, MainRepository::getModelNames($this->getDoctrine()));
+
+        return $gui->renderView("index.html.twig", ['models' => $models]);
     }
 }
